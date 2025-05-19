ec74e4e499530a16e9b6165e831534f80fa2dd9d
asyaisbil <asyaisbill@gmail.com>
2025-05-19 22:45:04 +0300

    Project_4

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './components/Layout.css';
-import Products from './components/Products';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Offer from './components/Offer';
+/* ADIM 2? */
 
 function App() {
   return (
     <>
       <Header />
       <div className="content-section">
-        <Products />
+        <Offer />
       </div>
       <Footer />
     </>
